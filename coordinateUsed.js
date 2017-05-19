bee4223e4b7d8551f52b79fd3e201036eefbb3c4
Keith Wong <keith.wong3672@gmail.com>
2017-05-19 08:50:45 -0700

    Create module from function coordinateUsed

---
+++ coordinateUsed.js
@@ -0,0 +1,11 @@
+
+// function that checks to see if current coordinate has already been used in current chain
+module.exports = (grid, row, column, chain) => {
+  let unused = true;
+  const key = '' + row + column;
+  // if value in object equal to value in cell, the cell has been used
+  if (chain[key] === grid[row][column]) {
+    unused = false;
+  }
+  return unused;
+};
