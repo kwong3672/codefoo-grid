df99d60811f0d3527ef492a90af2310ef87cf894
Keith Wong <keith.wong3672@gmail.com>
2017-05-19 08:49:18 -0700

    Move coordinateUsed function to new file

--- grid.js
+++ grid.js
@@ -1,8 +1,9 @@
 const createGrid = require('./createGrid.js');
+const coordinateUsed = require('./coordinateUsed.js');
 
-var gridArea = 0;
-var gridHeight = 0;
-var gridWidth = 0;
+var gridArea = 9;
+var gridHeight = 3;
+var gridWidth = 3;
 var validAnswers = {};
 
 // function that create a chain of cells and checks if the value inside adds up to a valid answer
@@ -29,33 +30,33 @@ var createChain = function (row, column, chainSum, chain) {
     }
 
     // checkAdjacent(row, column, chainSum, chain);
-    checkCoordinate(row - 1, column, chain, Object.assign({}, chain));
+    checkCoordinate(grid, row - 1, column, chain, Object.assign({}, chain));
     // check diagonal above and right
-    checkCoordinate(row - 1, column + 1, chainSum, Object.assign({}, chain));
+    checkCoordinate(grid, row - 1, column + 1, chainSum, Object.assign({}, chain));
     // check right
-    checkCoordinate(row, column + 1, chainSum, Object.assign({}, chain));
+    checkCoordinate(grid, row, column + 1, chainSum, Object.assign({}, chain));
     // check diagonal below and right
-    checkCoordinate(row + 1, column + 1, chainSum, Object.assign({}, chain));
+    checkCoordinate(grid, row + 1, column + 1, chainSum, Object.assign({}, chain));
     // // check below
-    checkCoordinate(row + 1, column, chainSum, Object.assign({}, chain));
+    checkCoordinate(grid, row + 1, column, chainSum, Object.assign({}, chain));
     // check diagonal below and left
-    checkCoordinate(row + 1, column - 1, chainSum, Object.assign({}, chain));
+    checkCoordinate(grid, row + 1, column - 1, chainSum, Object.assign({}, chain));
     // check left
-    checkCoordinate(row, column - 1, chainSum, Object.assign({}, chain));
+    checkCoordinate(grid, row, column - 1, chainSum, Object.assign({}, chain));
     // check diagonal above and left
-    checkCoordinate(row - 1, column - 1, chainSum, Object.assign({}, chain));
+    checkCoordinate(grid, row - 1, column - 1, chainSum, Object.assign({}, chain));
   }
 };
 
 // function that checks if the coordinate is valid, unused and less than the area
-var checkCoordinate = function (row, column, chainSum, chain) {
+var checkCoordinate = function (grid, row, column, chainSum, chain) {
   // check if cell is valid coordinate if not valid stop
   if (!validCoordinate(row, column)) {
     return;
   }
 
   // check if cell has been used in current chain if used stop
-  if (!coordinateUsed(row, column, chain)) {
+  if (!coordinateUsed(grid, row, column, chain)) {
     return;
   }
 
@@ -76,17 +77,6 @@ var validCoordinate = function (row, column) {
     valid = false;
   }
   return valid;
-};
-
-// function that checks to see if current coordinate has already been used in current chain
-var coordinateUsed = function (row, column, chain) {
-  var unused = true;
-  var key = '' + row + column;
-  // if value in object equal to value in cell, the cell has been used
-  if (chain[key] === grid[row][column]) {
-    unused = false;
-  }
-  return unused;
 };
 
 // create and display grid change parameters to increase grid size
